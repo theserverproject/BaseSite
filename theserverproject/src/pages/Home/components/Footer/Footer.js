702513c48892375b817d8c20f04b8a4dd2d9e53d
Ross Alexandra <rossandus@hotmail.com>
2019-09-05 10:14:32 -0700

    Footer - Set title of web page to 'TheServerProject'

--- theserverproject/src/pages/Home/components/Footer/Footer.js
+++ theserverproject/src/pages/Home/components/Footer/Footer.js
@@ -4,8 +4,16 @@ import './Footer.css'
 const Footer = (props) => {
     return (
         <footer id="footer">
-            <p class="footer-text">This is a footer. What do people usually put here?</p>
-            <p class="footer-text">If you see anything buggy here, email it to: Ross-Alexandra@outlook.com</p>
+            <div className="footer-left-pane">
+                <p> className="footer-text">Something should live in the left pane...</p>
+            </div>
+            <div className="footer-center-pane">
+                <p className="footer-text">This is a footer. What do people usually put here?</p>
+                <p className="footer-text">If you see anything buggy here, email it to: Ross-Alexandra@outlook.com</p>
+            </div>
+            <div className="footer-right-pane">
+                <p className="footer-text">Socials usually go here... don't they?</p>
+            </div>
         </footer>
     );
 }
