95dc1c10306695f692c3e39a1504a00ea814ecf0
Ross Alexandra <rossandus@hotmail.com>
2019-09-05 12:35:28 -0700

    firstDraft - Images can now specify whether their links open in new tab.

--- theserverproject/src/minorComponents/image/Image.js
+++ theserverproject/src/minorComponents/image/Image.js
@@ -3,24 +3,37 @@ import './Image.css'
 
 const Image = (props) => {
 
+    // Fallback text classes
     let fallbackText = props.defaultText ? props.defaultText : "No Image Found";
     let fallbackTextClass = props.textClassOverride ? props.textClassOverride + ' text-fallback' : 'text-fallback';
 
+    // Image classes
     let imageAltText = props.altText ? props.altText : "";
     let imageClass = props.imageClassOverride ? props.imageClassOverride + ' image-image' : 'image-image';
-
     let classOverride = props.classOverride ? props.classOverride + ' image' : 'image';
 
-    return (
-        <div className={ classOverride }>
-            {   props.image ? (
-                    <img src={ props.image } alt={ imageAltText } className={ imageClass  }/>
-                ) : (
-                     <p className={ fallbackTextClass }> { fallbackText } </p>
-                )
-            }
-        </div>
-    );
+    if (!props.image) {
+        return <p className={ fallbackTextClass}>{fallbackText}</p>;
+
+    }
+
+    let imgHtml = <img src={ props.image } alt={ imageAltText } className={ imageClass }/>;
+
+    if (props.imageLink) {
+
+        // When target is set to _blank then the
+        // link will open in a new page. If its
+        // set to '', then it will open in the
+        // same page.
+        let target = '';
+        if (props.linkNewTab) {
+            target = '_blank';
+        }
+
+        return <a href={ props.imageLink } target={ target }>{imgHtml}</a>;
+    }
+
+    return imgHtml;
 }
 
 export default Image
--- theserverproject/src/pages/Home/components/Footer/Footer.js
+++ theserverproject/src/pages/Home/components/Footer/Footer.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import './Footer.css'
 
+// Import minor componenets needed.
+import Image from '../../../../minorComponents/image/Image.js'
+
 const Footer = (props) => {
     return (
         <footer id="footer">
             <div className="footer-left-pane">
-                <p> className="footer-text">Something should live in the left pane...</p>
+                <p className="footer-text">The Server Project by Ross Alexandra © 2019</p>
             </div>
             <div className="footer-center-pane">
-                <p className="footer-text">This is a footer. What do people usually put here?</p>
-                <p className="footer-text">If you see anything buggy here, email it to: Ross-Alexandra@outlook.com</p>
+                <p className="footer-text">Everything here is a work in progress, as such bugs may exist. Should you find a</p>
+                <p className="footer-text">bug, please email relavant information to: Ross-Alexandra@outlook.com</p>
             </div>
             <div className="footer-right-pane">
-                <p className="footer-text">Socials usually go here... don't they?</p>
+                <Image image="logo.png" imageLink="http://" />
             </div>
         </footer>
     );
