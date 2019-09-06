4fd20a39e81c75d7759d59217f4423a793b5404c
Ross Alexandra <rossandus@hotmail.com>
2019-09-06 14:48:42 -0700

    GH-13 - Projects tab now opens in same tab.

--- theserverproject/src/pages/Home/components/Navbar/Navbar.js
+++ theserverproject/src/pages/Home/components/Navbar/Navbar.js
@@ -17,7 +17,7 @@ const Navbar = () => {
     return (
         <header id="navbar">
             <Image classOverride= { 'navbar-header' } image="logo.png" imageLink="http://www.theserverproject.com" defaultText="Logo not found"/>
-            <a className="navbar-header" href="http://projects.theserverproject.com/" target='_blank' rel="noopener noreferrer">Projects</a>
+            <a className="navbar-header" href="http://projects.theserverproject.com/">Projects</a>
             <a className="navbar-header" href="https://www.slideshare.net/slideshow/embed_code/key/lfqoSUV9bb8kxs" target='_blank' rel="noopener noreferrer">Resume</a>
             <a className="navbar-header" href="https://www.github.com/Ross-Alexandra" target='_blank' rel="noopener noreferrer">GitHub</a>
             <Dropdown dropdownItems={ dropdownItems } dropdownClassOverride={ 'navbar-header' } dropdownTitleClassOverride={ "underline navbar-header" }>Others using this server</Dropdown>
