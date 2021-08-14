feed412e723ac1b84cfc637573ccccc8cf691e66
Ross Alexandra <Ross-Alexandra@outlook.com>
2021-08-13 21:13:57 -0700

    Re-created the navbar.

--- theserverproject/src/App.js
+++ theserverproject/src/App.js
@@ -1,23 +1,60 @@
-import React from 'react';
-import Navbar from './pages/Home/components/Navbar/Navbar';
-import Main from './pages/Home/components/Main/Main';
-import Footer from './pages/Home/components/Footer/Footer';
+import {Component} from 'react';
 
-// Import App CSS classes.
-import './AppCssColors.css';
+import logo from './assets/Logo.png';
 import './App.css';
 
-//Import Home Page CSS classes.
-import './pages/Home/HomePageVariables.css';
+class App extends Component {
 
-function App() {
-    return (
-        <div id="page-content">
-            <Navbar/>
-            <Main/>
-            <Footer/>
-        </div>
-    );
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            activeLink: "/"
+        }
+    }
+
+    navigate = (e) => {
+        e.preventDefault();
+
+        this.setState({activeLink: e.target.pathname});
+    }
+
+    navigate
+
+    render() {
+
+        const linkTree = [["/", "Home"], ["/projects", "Projects"]].map((value, indx, arr) => {
+            return (
+                value[0] === this.state.activeLink ? 
+                    <div className={"active-navigator-link"}>{value[1]}</div>
+                :
+                    <a className={"navigator-link"} href={value[0]} onClick={e => {this.navigate(e)}}>{value[1]}</a>
+            )
+        });
+
+        return (
+            <div className="app">
+                <div className="app-header">
+                    <div className="content-body header-content">
+                        <img className="app-logo" src={logo} alt="Server Project"/>
+                        <nav className="app-navigator">
+                            {linkTree}
+                        </nav>
+                    </div>
+                </div>
+                <div className="app-body">
+                    <div className="content-body">
+
+                    </div>
+                </div>
+                <div className="app-footer">
+                    <div className="content-body">
+
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
