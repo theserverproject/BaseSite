321ba3021f811a6e200288bb7b08d55f7418180f
Ross Alexandra <Ross-Alexandra@outlook.com>
2021-08-13 22:42:12 -0700

    recreated basic landing page.

--- theserverproject/src/App.js
+++ theserverproject/src/App.js
@@ -1,6 +1,7 @@
 import {Component} from 'react';
 
 import logo from './assets/Logo.png';
+import headshot from './assets/Headshot.jpg'
 import './App.css';
 
 class App extends Component {
@@ -9,26 +10,26 @@ class App extends Component {
         super(props);
 
         this.state = {
-            activeLink: "/"
+            activeLink: "Home"
         }
     }
 
     navigate = (e) => {
         e.preventDefault();
 
-        this.setState({activeLink: e.target.pathname});
+        this.setState({activeLink: e.target.id});
     }
 
     navigate
 
     render() {
 
-        const linkTree = [["/", "Home"], ["/projects", "Projects"]].map((value, indx, arr) => {
+        const linkTree = ["Home", "Projects"].map((value, indx, arr) => {
             return (
-                value[0] === this.state.activeLink ? 
-                    <div className={"active-navigator-link"}>{value[1]}</div>
+                value === this.state.activeLink ? 
+                    <div className={"active-navigator-link"}>{value}</div>
                 :
-                    <a className={"navigator-link"} href={value[0]} onClick={e => {this.navigate(e)}}>{value[1]}</a>
+                    <div className={"navigator-link"} id={value} onClick={e => {this.navigate(e)}}>{value}</div>
             )
         });
 
@@ -44,12 +45,25 @@ class App extends Component {
                 </div>
                 <div className="app-body">
                     <div className="content-body">
-
+                        {/* Internal content. This should be extracted. */}
+                            <p className="content-title">Ross Alexandra</p>
+                            <img className="headshot" src={headshot} alt="missing"/>
+                            <div className="content">
+                                <p>Welcome to my portfolio!</p>
+                                <br/>
+                                <p>To learn more about this website, click on the edges of the page.</p>
+                                <p>To see my actual portfolio, click the link in the top right.</p>
+                            </div>
+                        {/* End of internal content.*/}
                     </div>
                 </div>
                 <div className="app-footer">
                     <div className="content-body">
-
+                        <div className="footer-content">
+                            <p>For any inquiries please feel free to contact me at 
+                                <a href="mailto:Ross-Alexandra@outlook.com"> Ross-Alexandra@outlook.com</a>
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
